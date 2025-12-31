417fa3314be8dcc2cf2ccc3a45473193584b2baf
Bos Eriko Reyes <bos@eriko.ph>
2025-12-31 14:23:40 +0800

    Add distrobox and podman to home packages

--- home.nix
+++ home.nix
@@ -5,6 +5,7 @@
   home.username = "deck";
   home.homeDirectory = "/home/deck";
 
+
   programs.bash = {
     enable = true;
     initExtra = ''
@@ -20,7 +21,9 @@
   home.stateVersion = "25.11"; # don't change this even if you upgrade your channel in the future, this should stay the same as the version you first installed nix on
 
   home.packages = with pkgs; [
-    vscode        # Visual Studio Code (https://github.com/BosEriko/vs)
+        vscode        # Visual Studio Code (https://github.com/BosEriko/vs)
+        distrobox
+        podman
   ];
 
   programs.home-manager.enable = true;
