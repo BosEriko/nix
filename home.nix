32f66316a7a1b72e4864ad5e84ce7fb4436cd6b8
Bos Eriko Reyes <bos@eriko.ph>
2026-02-09 12:05:09 +0800

    Add neovim to home packages

--- home.nix
+++ home.nix
@@ -21,7 +21,7 @@
   home.stateVersion = "25.11"; # don't change this even if you upgrade your channel in the future, this should stay the same as the version you first installed nix on
 
   home.packages = with pkgs; [
-        vscode        # Visual Studio Code (https://github.com/BosEriko/vs)
+        neovim
   ];
 
   programs.home-manager.enable = true;
