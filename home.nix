027a831343edfe3c41f1757de20952024d951359
Bos Eriko Reyes <bos@eriko.ph>
2026-02-09 12:35:56 +0800

    Improve bash init script and formatting in home.nix
    Refactor bash initialization and formatting in home.nix.

--- home.nix
+++ home.nix
@@ -1,20 +1,20 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   lazyvimDir = "${config.home.homeDirectory}/.config/nvim";
 in
-
 {
   nixpkgs.config.allowUnfree = true;
 
   home.username = "deck";
   home.homeDirectory = "/home/deck";
 
-
   programs.bash = {
     enable = true;
     initExtra = ''
-      if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
+      if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+        . $HOME/.nix-profile/etc/profile.d/nix.sh
+      fi
 
       export NIX_SHELL_PRESERVE_PROMPT=1
       if [[ -n "$IN_NIX_SHELL" ]]; then
@@ -23,16 +23,17 @@ in
     '';
   };
 
-  home.stateVersion = "25.11"; # don't change this even if you upgrade your channel in the future, this should stay the same as the version you first installed nix on
+  home.stateVersion = "25.11"; # don't change this even if you upgrade your channel in the future
 
   home.packages = with pkgs; [
-        neovim
+    neovim
   ];
 
   programs.home-manager.enable = true;
 
   # Automatically clone LazyVim if it's not already installed
-  home.activation.lazyvim = lib.mkIf !(builtins.pathExists lazyvimDir) ''
+  home.activation.lazyvim = lib.mkIf ( ! builtins.pathExists lazyvimDir ) ''
+    export PATH=${pkgs.git}/bin:$PATH
     echo "Installing LazyVim..."
     mkdir -p $HOME/.config/nvim
     git clone https://github.com/LazyVim/starter $HOME/.config/nvim
