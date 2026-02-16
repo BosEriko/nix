7cabf2214751a901d00ab7101bf7e925b5def877
BosEriko <bos@eriko.ph>
2026-02-16 10:27:30 +0800

    Feature: Add Tmux

--- home.nix
+++ home.nix
@@ -19,8 +19,10 @@ in
     waybar       # Window Manager: Status bar (https://github.com/BosEriko/sway)
     brave        # Browser: Brave (https://github.com/BosEriko/brave)
     foot         # Terminal: Foot (https://github.com/BosEriko/foot)
-    vscodium     # Editor: VSCodium (https://github.com/BosEriko/codium)
     neovim       # Editor: NeoVim - VSCodium Dependency (https://github.com/BosEriko/codium)
+
+    # Dependencies
+    tmux         # Used in ~/.files/modules/tmux/install.z.sh
   ];
 
   programs.home-manager.enable = true;
