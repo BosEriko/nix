4c824358aa6ff44119359502023e8ce6caaa0be7
BosEriko <bos@eriko.ph>
2026-02-23 12:38:56 +0800

    Feature: Remove Foot

--- home.nix
+++ home.nix
@@ -15,7 +15,6 @@ in
 
   home.packages = with pkgs; [
     brave                     # Browser: Brave (https://github.com/BosEriko/brave)
-    foot                      # Shell: Foot (https://github.com/BosEriko/foot)
   ];
 
   programs.home-manager.enable = true;
