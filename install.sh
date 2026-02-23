5defbf543746a506df1a4aca472bc1411de24007
BosEriko <bos@eriko.ph>
2026-02-23 12:33:05 +0800

    Feature: Install Paid Packages

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ VERSION=$(cat .nix-version)
 
 echo "Setting up Nix channels for version $VERSION"
 
-# Remove old channel
+# Remove Old Channel
 nix-channel --remove nixpkgs 2>/dev/null || true
 
-# Add new channel
+# Add New Channel
 nix-channel --add https://nixos.org/channels/nixos-$VERSION nixpkgs
 nix-channel --update
 
-# Installing Applications
-nix-env -iA nixpkgs.discord # Used in https://github.com/BosEriko/sway
-nix-env -iA nixpkgs.spotify # Used in https://github.com/BosEriko/sway
+# Install Applications
+NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.discord # Used in https://github.com/BosEriko/sway
+NIXPKGS_ALLOW_UNFREE=1 nix-env -iA nixpkgs.spotify # Used in https://github.com/BosEriko/sway
 
 echo "Done ✅"
